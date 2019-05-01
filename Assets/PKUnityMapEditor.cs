786ecd52dec99807d05bdc4f7a4c6afc93cd917b
Velorexe <david.bruschke@gmail.com>
2019-05-01 11:01:42 +0200

    Added start of Unity Project

---
+++ Assets/PKUnityMapEditor.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class PKUnityMapEditor : EditorWindow {
+
+    [MenuItem("Window/Pokemon Unity Map Editor")]
+    public static void ShowWindow()
+    {
+        GetWindow<PKUnityMapEditor>("Pokemon Unity Map Editor");
+    }
+
+    void OnGUI()
+    {
+        GUILayout.Label("Put your map here:");
+        string MapInput = EditorGUILayout.TextArea("Name", GUILayout.Height(position.height - 30));
+    }
+
+}
