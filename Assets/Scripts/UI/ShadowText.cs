9b73040674e53d45a7992e8d5140587d41bb3714
TeamPopplio <fatedclonerexworldcast@gmail.com>
2019-07-13 11:19:18 -0700

    Added new font and textures for UI in a separate scene, added temporary BGM.

---
+++ Assets/Scripts/UI/ShadowText.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class ShadowText : MonoBehaviour
+{
+    public Text text;
+    // Update is called once per frame
+    void Update()
+    {
+        gameObject.GetComponent<Text>().text = text.text;
+    }
+}
