1e6ed00993c99e043117321d1ecf7b16e8e7e8b6
Velorexe <david.bruschke@gmail.com>
2019-07-16 22:24:56 +0200

    Added TeamPopplio's UI to main Editor scene

--- Assets/Scripts/ColliderExport.cs
+++ Assets/Scripts/ColliderExport.cs
@@ -15,91 +15,91 @@ public class ColliderExport : MonoBehaviour
 
     public void CombineColliderMeshes()
     {
-        #region Collision
-        if (CollisionTransform.childCount > 0)
-        {
-            MeshFilter[] childMeshFilters = CollisionTransform.GetComponentsInChildren<MeshFilter>();
-            if (CollisionTransform.GetComponent<MeshFilter>().sharedMesh != null)
-                childMeshFilters[0] = CollisionTransform.GetComponent<MeshFilter>();
-            else
-                childMeshFilters = childMeshFilters.Skip(1).ToArray();
-            CombineInstance[] combine = new CombineInstance[childMeshFilters.Length];
+        //#region Collision
+        //if (CollisionTransform.childCount > 0)
+        //{
+        //    MeshFilter[] childMeshFilters = CollisionTransform.GetComponentsInChildren<MeshFilter>();
+        //    if (CollisionTransform.GetComponent<MeshFilter>().sharedMesh != null)
+        //        childMeshFilters[0] = CollisionTransform.GetComponent<MeshFilter>();
+        //    else
+        //        childMeshFilters = childMeshFilters.Skip(1).ToArray();
+        //    CombineInstance[] combine = new CombineInstance[childMeshFilters.Length];
 
-            for (int i = 0; i < childMeshFilters.Length; i++)
-            {
-                combine[i].mesh = childMeshFilters[i].sharedMesh;
-                combine[i].transform = childMeshFilters[i].transform.localToWorldMatrix;
-            }
+        //    for (int i = 0; i < childMeshFilters.Length; i++)
+        //    {
+        //        combine[i].mesh = childMeshFilters[i].sharedMesh;
+        //        combine[i].transform = childMeshFilters[i].transform.localToWorldMatrix;
+        //    }
 
-            CollisionTransform.GetComponent<MeshFilter>().mesh = new Mesh();
-            CollisionTransform.GetComponent<MeshFilter>().mesh.CombineMeshes(combine, true, true);
+        //    CollisionTransform.GetComponent<MeshFilter>().mesh = new Mesh();
+        //    CollisionTransform.GetComponent<MeshFilter>().mesh.CombineMeshes(combine, true, true);
 
-            CollisionTransform.GetComponent<MeshRenderer>().sharedMaterial = CollisionMaterial;
+        //    CollisionTransform.GetComponent<MeshRenderer>().sharedMaterial = CollisionMaterial;
 
-            foreach (Transform childObject in CollisionTransform)
-            {
-                if (childObject != CollisionTransform)
-                    Destroy(childObject.gameObject);
-            }
-        }
-        #endregion
+        //    foreach (Transform childObject in CollisionTransform)
+        //    {
+        //        if (childObject != CollisionTransform)
+        //            Destroy(childObject.gameObject);
+        //    }
+        //}
+        //#endregion
 
-        #region Water
-        if (WaterTransform.childCount > 0)
-        {
-            MeshFilter[] childMeshFilters = WaterTransform.GetComponentsInChildren<MeshFilter>();
-            if (WaterTransform.GetComponent<MeshFilter>().sharedMesh != null)
-                childMeshFilters[0] = WaterTransform.GetComponent<MeshFilter>();
-            else
-                childMeshFilters = childMeshFilters.Skip(1).ToArray();
-            CombineInstance[] combine = new CombineInstance[childMeshFilters.Length];
+        //#region Water
+        //if (WaterTransform.childCount > 0)
+        //{
+        //    MeshFilter[] childMeshFilters = WaterTransform.GetComponentsInChildren<MeshFilter>();
+        //    if (WaterTransform.GetComponent<MeshFilter>().sharedMesh != null)
+        //        childMeshFilters[0] = WaterTransform.GetComponent<MeshFilter>();
+        //    else
+        //        childMeshFilters = childMeshFilters.Skip(1).ToArray();
+        //    CombineInstance[] combine = new CombineInstance[childMeshFilters.Length];
 
-            for (int i = 0; i < childMeshFilters.Length; i++)
-            {
-                combine[i].mesh = childMeshFilters[i].sharedMesh;
-                combine[i].transform = childMeshFilters[i].transform.localToWorldMatrix;
-            }
+        //    for (int i = 0; i < childMeshFilters.Length; i++)
+        //    {
+        //        combine[i].mesh = childMeshFilters[i].sharedMesh;
+        //        combine[i].transform = childMeshFilters[i].transform.localToWorldMatrix;
+        //    }
 
-            WaterTransform.GetComponent<MeshFilter>().mesh = new Mesh();
-            WaterTransform.GetComponent<MeshFilter>().mesh.CombineMeshes(combine, true, true);
+        //    WaterTransform.GetComponent<MeshFilter>().mesh = new Mesh();
+        //    WaterTransform.GetComponent<MeshFilter>().mesh.CombineMeshes(combine, true, true);
 
-            WaterTransform.GetComponent<MeshRenderer>().sharedMaterial = WaterMaterial;
+        //    WaterTransform.GetComponent<MeshRenderer>().sharedMaterial = WaterMaterial;
 
-            foreach (Transform childObject in WaterTransform)
-            {
-                if (childObject != WaterTransform)
-                    Destroy(childObject.gameObject);
-            }
-        }
-        #endregion
+        //    foreach (Transform childObject in WaterTransform)
+        //    {
+        //        if (childObject != WaterTransform)
+        //            Destroy(childObject.gameObject);
+        //    }
+        //}
+        //#endregion
 
-        #region Walkable
-        if (WalkableTransform.childCount > 0)
-        {
-            MeshFilter[] childMeshFilters = WalkableTransform.GetComponentsInChildren<MeshFilter>();
-            if (WalkableTransform.GetComponent<MeshFilter>().sharedMesh != null)
-                childMeshFilters[0] = WalkableTransform.GetComponent<MeshFilter>();
-            else
-                childMeshFilters = childMeshFilters.Skip(1).ToArray();
-            CombineInstance[] combine = new CombineInstance[childMeshFilters.Length];
+        //#region Walkable
+        //if (WalkableTransform.childCount > 0)
+        //{
+        //    MeshFilter[] childMeshFilters = WalkableTransform.GetComponentsInChildren<MeshFilter>();
+        //    if (WalkableTransform.GetComponent<MeshFilter>().sharedMesh != null)
+        //        childMeshFilters[0] = WalkableTransform.GetComponent<MeshFilter>();
+        //    else
+        //        childMeshFilters = childMeshFilters.Skip(1).ToArray();
+        //    CombineInstance[] combine = new CombineInstance[childMeshFilters.Length];
 
-            for (int i = 0; i < childMeshFilters.Length; i++)
-            {
-                combine[i].mesh = childMeshFilters[i].sharedMesh;
-                combine[i].transform = childMeshFilters[i].transform.localToWorldMatrix;
-            }
+        //    for (int i = 0; i < childMeshFilters.Length; i++)
+        //    {
+        //        combine[i].mesh = childMeshFilters[i].sharedMesh;
+        //        combine[i].transform = childMeshFilters[i].transform.localToWorldMatrix;
+        //    }
 
-            WalkableTransform.GetComponent<MeshFilter>().mesh = new Mesh();
-            WalkableTransform.GetComponent<MeshFilter>().mesh.CombineMeshes(combine, true, true);
+        //    WalkableTransform.GetComponent<MeshFilter>().mesh = new Mesh();
+        //    WalkableTransform.GetComponent<MeshFilter>().mesh.CombineMeshes(combine, true, true);
 
-            WalkableTransform.GetComponent<MeshRenderer>().sharedMaterial = WalkableMaterial;
+        //    WalkableTransform.GetComponent<MeshRenderer>().sharedMaterial = WalkableMaterial;
 
-            foreach (Transform childObject in WalkableTransform)
-            {
-                if (childObject != WalkableTransform)
-                    Destroy(childObject.gameObject);
-            }
-        }
-        #endregion
+        //    foreach (Transform childObject in WalkableTransform)
+        //    {
+        //        if (childObject != WalkableTransform)
+        //            Destroy(childObject.gameObject);
+        //    }
+        //}
+        //#endregion
     }
 }
